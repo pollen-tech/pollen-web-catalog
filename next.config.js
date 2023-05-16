182f2c7baec01cac3a41d68c1dca06eb3599bd65
Septian A Tama <sfirst.tama@gmail.com>
2023-05-16 14:10:45 +0700

    setup lodash and configure modularizeImports

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  modularizeImports: {
+    lodash: {
+      transform: 'lodash/{{member}}',
+      preventFullImport: true,
+    },
+  },
+}
 
 module.exports = nextConfig
