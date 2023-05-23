76b8dd972b1108259370725b581b93ba86ced0aa
Septian A Tama <sfirst.tama@gmail.com>
2023-05-24 02:46:26 +0700

    initiate catalog page

---
+++ src/app/catalog/page.tsx
@@ -0,0 +1,11 @@
+import { Card } from '~/components/common/card'
+
+export default function CatalogPage() {
+  return (
+    <div className="catalog-page container mx-auto">
+      <Card className="my-8">
+        <h1 className="text-3xl font-bold">Catalog Page</h1>
+      </Card>
+    </div>
+  )
+}
