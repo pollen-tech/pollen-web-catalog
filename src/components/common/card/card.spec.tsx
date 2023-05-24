e8e7b3a0e14e1714ab62524e04491300902b8ce7
Septian A Tama <sfirst.tama@gmail.com>
2023-05-24 09:58:26 +0700

    add sample tests for button component

--- src/components/common/card/card.spec.tsx
+++ src/components/common/card/card.spec.tsx
@@ -2,7 +2,7 @@ import { render, screen } from '@testing-library/react'
 import { Card } from './'
 
 describe('Card', () => {
-  it('should renders a content within card', () => {
+  it('should renders a content passed as children', () => {
     render(
       <Card>
         <h1>Hello</h1>
