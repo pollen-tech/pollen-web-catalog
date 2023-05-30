9e707828405e322a84cb41549d0b2505c9a4181b
Septian A Tama <sfirst.tama@gmail.com>
2023-05-30 19:29:15 +0700

    create ui for user menu

--- src/components/navigation/navbar-menu/navbar-menu.tsx
+++ src/components/navigation/navbar-menu/navbar-menu.tsx
@@ -1,8 +1,39 @@
 'use client'
 
 import * as Menubar from '@radix-ui/react-menubar'
+import * as Avatar from '@radix-ui/react-avatar'
 
-import { BellIcon, UserIcon } from '@heroicons/react/24/outline'
+import {
+  BellIcon,
+  Cog8ToothIcon,
+  CurrencyDollarIcon,
+  TruckIcon,
+  UserIcon,
+} from '@heroicons/react/24/outline'
+import { ArrowRightOnRectangleIcon } from '@heroicons/react/24/solid'
+
+const USER_MENU_ITEMS = [
+  {
+    id: 'offer',
+    label: 'Offer',
+    icon: CurrencyDollarIcon,
+  },
+  {
+    id: 'order',
+    label: 'Order (Coming Soon)',
+    icon: TruckIcon,
+  },
+  {
+    id: 'settings',
+    label: 'Settings',
+    icon: Cog8ToothIcon,
+  },
+  {
+    id: 'logout',
+    label: 'Logout',
+    icon: ArrowRightOnRectangleIcon,
+  },
+]
 
 export function NavbarMenu() {
   return (
@@ -30,20 +61,32 @@ export function NavbarMenu() {
         </Menubar.Trigger>
         <Menubar.Portal>
           <Menubar.Content
-            className="rounded bg-white p-4 drop-shadow"
+            className="w-[298px] bg-gray-50 drop-shadow"
             align="end"
             sideOffset={10}
             alignOffset={-10}
           >
-            <Menubar.Item className="cursor-pointer p-2 text-gray-600 hover:bg-gray-50">
-              Profile
-            </Menubar.Item>
-            <Menubar.Item className="cursor-pointer p-2 text-gray-600 hover:bg-gray-50">
-              Settings
-            </Menubar.Item>
-            <Menubar.Item className="cursor-pointer p-2 text-gray-600 hover:bg-gray-50">
-              Logout
-            </Menubar.Item>
+            <div className="mb-2 flex items-center rounded-t-lg bg-white px-2 py-4">
+              <Avatar.Root>
+                <Avatar.Fallback className="mx-4 flex h-8 w-8 items-center justify-center rounded-full bg-purple-50 text-pollen-purple">
+                  B
+                </Avatar.Fallback>
+              </Avatar.Root>
+              <div className="flex flex-col">
+                <span className="text-sm text-gray-900">Buyer User</span>
+                <span className="text-xs text-gray-700">Buyer Company</span>
+              </div>
+            </div>
+            <div className="rounded-b-lg bg-white p-2">
+              {USER_MENU_ITEMS.map(({ id, label, icon: Icon }) => (
+                <Menubar.Item
+                  className="flex cursor-pointer items-center px-4 py-2 text-sm text-gray-600 hover:border-none hover:bg-purple-50 hover:text-gray-900"
+                  key={id}
+                >
+                  <Icon className="mr-4 h-4 w-4" /> {label}
+                </Menubar.Item>
+              ))}
+            </div>
           </Menubar.Content>
         </Menubar.Portal>
       </Menubar.Menu>
