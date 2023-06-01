3142fc6a82a2a857f8da877358c639247f3ee7db
Septian A Tama <sfirst.tama@gmail.com>
2023-06-01 19:57:12 +0700

    extract notification menu as separated component file

--- src/components/navigation/navbar-menu/navbar-menu.tsx
+++ src/components/navigation/navbar-menu/navbar-menu.tsx
@@ -1,95 +1,45 @@
 'use client'
 
-import * as Menubar from '@radix-ui/react-menubar'
-import * as Avatar from '@radix-ui/react-avatar'
-import * as Tabs from '@radix-ui/react-tabs'
+import { Root as MenuRoot } from '@radix-ui/react-menubar'
 
-import { BellIcon } from '@heroicons/react/24/outline'
-
-import {
-  CurrencyDollarIcon as DollarSolidIcon,
-  ArchiveBoxIcon,
-  TruckIcon as TruckSolidIcon,
-  MapPinIcon,
-} from '@heroicons/react/24/solid'
 import { UserMenu } from './user-menu'
+import { Notifications } from './notifications'
 
-const NOTIFICATION_TABS = [
+const dummyNotifs = [
   {
-    id: 'notif-offer',
-    label: 'Offer',
-    icon: DollarSolidIcon,
+    id: 'notif-1',
+    title: 'Your offer has been sent to seller',
+    content:
+      'Your offer to catalog “P1 Near Expiry Food Products” has been sent.',
+    date: '2021-08-01T00:00:00.000Z',
+    read: false,
+    category: 'notif-offer',
   },
   {
-    id: 'notif-order',
-    label: 'Order (Coming Soon)',
-    icon: ArchiveBoxIcon,
+    id: 'notif-2',
+    title: 'Your order has been processed',
+    content:
+      'Your offer to catalog “P1 Near Expiry Food Products” has been processed.',
+    date: '2021-08-01T00:00:00.000Z',
+    read: false,
+    category: 'notif-order',
   },
   {
-    id: 'notif-shipping',
-    label: 'Shipping',
-    icon: TruckSolidIcon,
-  },
-  {
-    id: 'notif-completed',
-    label: 'Completed',
-    icon: MapPinIcon,
+    id: 'notif-3',
+    title: 'Your offer has been sent to seller',
+    content:
+      'Your offer to catalog “P2 Near Expiry Food Products” has been sent.',
+    date: '2021-08-01T00:00:00.000Z',
+    read: false,
+    category: 'notif-offer',
   },
 ]
 
 export function NavbarMenu() {
   return (
-    <Menubar.Root className="flex items-center gap-x-8">
-      <Menubar.Menu>
-        <Menubar.Trigger aria-label="notifications">
-          <BellIcon className="h-6 w-6 text-gray-600" />
-        </Menubar.Trigger>
-        <Menubar.Portal>
-          <Menubar.Content align="end">
-            <Menubar.Content
-              className="w-[400px] rounded-lg bg-gray-50 drop-shadow"
-              align="end"
-              sideOffset={10}
-              alignOffset={-10}
-            >
-              <div className="rounded-t-lg bg-white px-6 py-4 font-semibold text-pollen-purple">
-                Notifications
-              </div>
-              <Tabs.Root
-                className="bg-white px-6 py-4"
-                defaultValue="notif-offer"
-              >
-                <Tabs.List
-                  className="mb-2 flex items-center justify-between"
-                  aria-label="Notifications"
-                >
-                  {NOTIFICATION_TABS.map(({ id, label, icon: Icon }) => (
-                    <Tabs.Trigger
-                      className="flex shrink-0 grow flex-col items-center gap-4 text-gray-400 hover:text-pollen-purple"
-                      value={id}
-                      key={id}
-                    >
-                      <Icon className="h-6 w-6" />
-                      <span className="text-xs"> {label}</span>
-                    </Tabs.Trigger>
-                  ))}
-                </Tabs.List>
-                <div className="rounded-b-lg pt-4">
-                  {NOTIFICATION_TABS.map(({ id }) => (
-                    <Tabs.Content value={id} key={`content-${id}`}>
-                      {/* empty nofitications */}
-                      <div className="w-full rounded-b-lg py-10 text-center text-xs">
-                        No notifications yet
-                      </div>
-                    </Tabs.Content>
-                  ))}
-                </div>
-              </Tabs.Root>
-            </Menubar.Content>
-          </Menubar.Content>
-        </Menubar.Portal>
-      </Menubar.Menu>
+    <MenuRoot className="flex items-center gap-x-8">
+      <Notifications items={dummyNotifs} />
       <UserMenu id="buyer-id" username="Buyer Name" company="Buyer company" />
-    </Menubar.Root>
+    </MenuRoot>
   )
 }
---
+++ src/components/navigation/navbar-menu/notifications.tsx
@@ -0,0 +1,140 @@
+import { Menu, Trigger, Content, Portal, Item } from '@radix-ui/react-menubar'
+
+import * as Tabs from '@radix-ui/react-tabs'
+
+import { BellIcon } from '@heroicons/react/24/outline'
+import {
+  CurrencyDollarIcon as DollarSolidIcon,
+  ArchiveBoxIcon,
+  TruckIcon as TruckSolidIcon,
+  MapPinIcon,
+} from '@heroicons/react/24/solid'
+
+import { format } from 'date-fns'
+
+type TNotificationItem = {
+  id: string
+  title: string
+  content: string
+  date: string
+  read: boolean
+  category: string
+}
+
+type TNotificationsProps = {
+  items?: TNotificationItem[]
+}
+
+const NOTIFICATION_TABS = [
+  {
+    category: 'notif-offer',
+    label: 'Offer',
+    icon: DollarSolidIcon,
+  },
+  {
+    category: 'notif-order',
+    label: 'Order (Coming Soon)',
+    icon: ArchiveBoxIcon,
+  },
+  {
+    category: 'notif-shipping',
+    label: 'Shipping',
+    icon: TruckSolidIcon,
+  },
+  {
+    category: 'notif-completed',
+    label: 'Completed',
+    icon: MapPinIcon,
+  },
+]
+
+export function NotificationsTabContent({
+  category,
+  items,
+}: {
+  category: string
+  items: TNotificationItem[]
+}) {
+  return (
+    <Tabs.Content value={category} key={`content-${category}`}>
+      <div className="divide-y divide-gray-200">
+        {items.length ? (
+          items.map((item) => (
+            // TODO: update to use Link from next/link, on add event handler, depends on the design
+            <Item
+              className="cursor-pointer px-6 py-5 hover:bg-purple-50"
+              key={item.id}
+              onSelect={() => console.log('selected', item.id)}
+            >
+              <div className="mb-2 flex justify-between text-xs">
+                <span className="font-semibold text-gray-900">
+                  {item.title}
+                </span>{' '}
+                <span className="text-gray-500">
+                  {/* TODO: update to relative format */}
+                  {format(new Date(item.date), 'MMM dd')}
+                </span>
+              </div>
+              <div className="text-xs text-gray-700">
+                {item.content} Click to see details.
+              </div>
+            </Item>
+          ))
+        ) : (
+          <div className="w-full rounded-b-lg py-10 text-center text-xs">
+            No notifications yet
+          </div>
+        )}
+      </div>
+    </Tabs.Content>
+  )
+}
+
+export function Notifications({ items = [] }: TNotificationsProps) {
+  return (
+    <Menu>
+      <Trigger aria-label="notifications">
+        <BellIcon className="h-6 w-6 text-gray-600" />
+      </Trigger>
+      <Portal>
+        <Content align="end">
+          <Content
+            className="w-[400px] rounded-lg bg-gray-50 drop-shadow"
+            align="end"
+            sideOffset={10}
+            alignOffset={-10}
+          >
+            <div className="rounded-t-lg bg-white px-6 py-4 font-semibold text-pollen-purple">
+              Notifications
+            </div>
+            <Tabs.Root defaultValue="notif-offer">
+              <Tabs.List
+                className="mb-2 flex items-center justify-between bg-white px-6 py-4"
+                aria-label="Notifications"
+              >
+                {NOTIFICATION_TABS.map(({ category, label, icon: Icon }) => (
+                  <Tabs.Trigger
+                    className="flex shrink-0 grow flex-col items-center gap-4 text-gray-400 hover:text-pollen-purple data-[state=active]:text-pollen-purple"
+                    value={category}
+                    key={category}
+                  >
+                    <Icon className="h-6 w-6" />
+                    <span className="text-xs"> {label}</span>
+                  </Tabs.Trigger>
+                ))}
+              </Tabs.List>
+              <div className="mt-1 rounded-b-lg bg-white pt-4">
+                {NOTIFICATION_TABS.map(({ category }) => (
+                  <NotificationsTabContent
+                    category={category}
+                    items={items.filter((item) => item.category === category)}
+                  />
+                ))}
+              </div>
+            </Tabs.Root>
+          </Content>
+        </Content>
+      </Portal>
+    </Menu>
+  )
+}
