d50278e57bf3fdbfbf38ced6bc141051c31ff731
Septian A Tama <sfirst.tama@gmail.com>
2023-06-01 20:52:14 +0700

    add test for navbar-menu component

---
+++ src/components/navigation/navbar-menu/navbar-menu.spec.tsx
@@ -0,0 +1,32 @@
+import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+
+import { NavbarMenu } from '.'
+
+describe('NavbarMenu', () => {
+  it('should renders two menuitem', () => {
+    render(<NavbarMenu />)
+
+    const menuItems = screen.getAllByRole('menuitem')
+
+    expect(menuItems).toHaveLength(2)
+  })
+
+  it('should renders a navbar with user menu, with default data-state as `closed`', () => {
+    render(<NavbarMenu />)
+
+    const userMenu = screen.getByLabelText('user menu')
+
+    expect(userMenu).toBeInTheDocument()
+    expect(userMenu.getAttribute('data-state')).toEqual('closed')
+  })
+
+  it('should renders a navbar with notifications menu', () => {
+    render(<NavbarMenu />)
+
+    const notifications = screen.getByLabelText('notifications')
+
+    expect(notifications).toBeInTheDocument()
+    expect(notifications.getAttribute('data-state')).toEqual('closed')
+  })
+})
