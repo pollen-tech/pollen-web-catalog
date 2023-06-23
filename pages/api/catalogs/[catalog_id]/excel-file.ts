60fd8128c7e1123796b823ca0ae914589f0bfbc9
Echa Pangersa Sugianto Oeoen <andyepso@gmail.com>
2023-06-23 19:54:07 +0700

    fix deployment

--- pages/api/catalogs/[catalog_id]/excel-file.ts
+++ pages/api/catalogs/[catalog_id]/excel-file.ts
@@ -4,23 +4,23 @@ import { config } from '~/config'
 import { getCookie } from 'cookies-next'
 import { ID_TOKEN_COOKIE_KEY } from '../../auth/constant'
 
+const proxy = createProxyMiddleware({
+  target: config.lms.endpoint,
+  secure: false,
+  pathRewrite: {
+    // change path proxy to the lms endpoint
+    "api": ``,
+  },
+  headers: {
+    API_KEY: config.lms.apiKey,
+  },
+})
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
   const cookie = getCookie(ID_TOKEN_COOKIE_KEY, { req, res })
   const path = req.url as string
   if (!cookie?.toString()) {
     return res.redirect(`/api/auth/login?returnTo=/${path}`)
   }
-  const proxy = createProxyMiddleware({
-    target: config.lms.endpoint,
-    secure: false,
-    pathRewrite: {
-      // change path proxy to the lms endpoint
-      [path]: `/catalogs/export-data/${req.query.catalog_id as string}`,
-    },
-    headers: {
-      API_KEY: config.lms.apiKey,
-    },
-  })
 
   proxy(req, res, (error: Error) => {
     res.status(500).end('Proxy Error')
