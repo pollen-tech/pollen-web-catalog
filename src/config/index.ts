c370d4486db444d04c61ce12a49d917ae32729d3
Echa Pangersa Sugianto Oeoen <andyepso@gmail.com>
2023-07-03 18:11:07 +0700

    fix: marketing url

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,7 +2,7 @@ export const config = {
   appsync: {
     endpoint: process.env.NEXT_PUBLIC_APPSYNC_ENDPOINT,
   },
-  marketingUrl: process.env.MARKETING_URL,
+  marketingUrl: process.env.LMS_MARKETING_URL,
   lms: {
     endpoint: process.env.LMS_ENDPOINT as string,
     apiKey: process.env.LMS_API_KEY as string,
