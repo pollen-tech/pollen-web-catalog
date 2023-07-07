d6e4eaae971f96c6bf6a6e29e92d14a08f7a30b2
Ilham fadhilah <r.ilhamfadhilah@gmail.com>
2023-07-07 15:22:52 +0700

    prepare gql

---
+++ src/services/sellers/sellers.ts
@@ -0,0 +1,20 @@
+import type { Seller } from '@pollen-tech/appsync-schema'
+
+import { query } from '~/lib/client'
+import { FETCH_SELLERS } from './query.gql'
+import { type Paginated } from '../generic/pagination'
+
+export interface TQuerySellersResponse {
+  sellers: Paginated<Seller>
+}
+export async function fetchSellers(search = '', page = 1, size = 10) {
+  const response = await query<TQuerySellersResponse>({
+    query: FETCH_SELLERS,
+    variables: {
+      page,
+      size,
+      search,
+    },
+  })
+  return response.data.sellers
+}
