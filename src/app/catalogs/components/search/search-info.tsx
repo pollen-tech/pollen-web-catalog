fc4a990369d05f7a217f1687b9ba1c72e140336d
Ilham fadhilah <r.ilhamfadhilah@gmail.com>
2023-07-07 10:49:31 +0700

    feat: filter seller list

--- src/app/catalogs/components/search/search-info.tsx
+++ src/app/catalogs/components/search/search-info.tsx
@@ -1,8 +1,10 @@
 'use client'
 import * as Popover from '@radix-ui/react-popover'
+import * as ScrollArea from '@radix-ui/react-scroll-area'
 import { type FormEventHandler, useState, type MouseEventHandler } from 'react'
 import { Card } from '~/components/common/card'
 import { useRouter as useNextRouter } from '~/hooks/router'
+import style from './search-info.module.css'
 
 const filterList = [
   { id: 1, val: 'Last Updated', sort: 'updatedAt', sortDirection: 'asc' },
@@ -48,6 +50,62 @@ const SortOption = ({
     >
       {val}
     </button>
+  )
+}
+
+const SellerSearch = ({}) => {
+  return (
+    <div className="relative mb-3 w-full">
+      <input
+        data-testid="search-field"
+        type="text"
+        className="block w-full rounded-lg border border-gray-300 bg-gray-100 p-2.5 pr-10 text-sm text-gray-900 focus:border-purple-600 focus:ring-purple-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-purple-500 dark:focus:ring-purple-500"
+        placeholder="Find a Seller"
+        required
+        onChange={({ target: { value } }) => value}
+      />
+      <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-3">
+        <svg
+          aria-hidden="true"
+          className="h-5 w-5 text-gray-500 dark:text-gray-400"
+          fill="currentColor"
+          viewBox="0 0 20 20"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            fillRule="evenodd"
+            d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
+            clipRule="evenodd"
+          ></path>
+        </svg>
+      </div>
+    </div>
+  )
+}
+
+const SellerList = ({}) => {
+  const data = Array.from({ length: 50 })
+  return (
+    <>
+      {data
+        .map((_, i, a) => `v1.2.0-beta.${a.length - i}`)
+        .map((tag, i) => (
+          <>
+            <div className="my-2 flex items-center">
+              <input
+                type="checkbox"
+                className="h-5 w-5 rounded border-gray-300 text-purple-600 accent-purple-600 focus:ring-purple-500 dark:border-gray-600 dark:text-white dark:focus:border-purple-500"
+              />
+              <label className="ml-4 text-gray-700 dark:text-gray-300">
+                Example Seller
+              </label>
+            </div>
+            {i !== data.length - 1 && (
+              <hr className="h-px border-0 bg-gray-200 dark:bg-gray-700"></hr>
+            )}
+          </>
+        ))}
+    </>
   )
 }
 
@@ -140,7 +198,10 @@ export function SearchInfo() {
                 </svg>
               </Popover.Trigger>
               <Popover.Portal>
-                <Popover.Content className="w-80 rounded border border-slate-300 bg-white p-4">
+                <Popover.Content
+                  sideOffset={10}
+                  className="w-80 rounded border border-slate-300 bg-white p-4"
+                >
                   {filterList.map((d, i) => (
                     <>
                       <SortOption
@@ -156,24 +217,58 @@ export function SearchInfo() {
                 </Popover.Content>
               </Popover.Portal>
             </Popover.Root>
-
-            <button className="focus:shadow-outline mx-1 inline-flex  h-10 items-center rounded-lg border border-slate-300 px-5 transition-colors duration-150  hover:border-slate-100 hover:bg-purple-600 hover:text-white  sm:w-full  md:w-28">
-              <span>Filter</span>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 20 20"
-                strokeWidth="1.5"
-                stroke="currentColor"
-                className="ml-3 h-4 w-4 fill-current"
+            <Popover.Root>
+              <Popover.Trigger
+                data-testid="sort-options-button"
+                className="focus:shadow-outline mx-1 inline-flex  h-10 items-center rounded-lg border border-slate-300 px-5 transition-colors duration-150  hover:border-slate-100 hover:bg-purple-600 hover:text-white  sm:w-full  md:w-28"
               >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M12 3c2.755 0 5.455.232 8.083.678.533.09.917.556.917 1.096v1.044a2.25 2.25 0 01-.659 1.591l-5.432 5.432a2.25 2.25 0 00-.659 1.591v2.927a2.25 2.25 0 01-1.244 2.013L9.75 21v-6.568a2.25 2.25 0 00-.659-1.591L3.659 7.409A2.25 2.25 0 013 5.818V4.774c0-.54.384-1.006.917-1.096A48.32 48.32 0 0112 3z"
-                />
-              </svg>
-            </button>
+                <span>Filter</span>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 20 20"
+                  strokeWidth="1.5"
+                  stroke="currentColor"
+                  className="ml-3 h-4 w-4 fill-current"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M12 3c2.755 0 5.455.232 8.083.678.533.09.917.556.917 1.096v1.044a2.25 2.25 0 01-.659 1.591l-5.432 5.432a2.25 2.25 0 00-.659 1.591v2.927a2.25 2.25 0 01-1.244 2.013L9.75 21v-6.568a2.25 2.25 0 00-.659-1.591L3.659 7.409A2.25 2.25 0 013 5.818V4.774c0-.54.384-1.006.917-1.096A48.32 48.32 0 0112 3z"
+                  />
+                </svg>
+              </Popover.Trigger>
+              <Popover.Portal>
+                <Popover.Content
+                  sideOffset={10}
+                  className={`max-h-[500px] w-80 rounded border border-slate-300 bg-white`}
+                >
+                  <ScrollArea.Root className={`${style['ScrollAreaRoot']}`}>
+                    <ScrollArea.Viewport
+                      className={`${style['ScrollAreaViewport']}`}
+                    >
+                      <div style={{ padding: '15px 20px' }}>
+                        <SellerSearch />
+                        <SellerList />
+                      </div>
+                    </ScrollArea.Viewport>
+                    <ScrollArea.Scrollbar
+                      className="ScrollAreaScrollbar"
+                      orientation="vertical"
+                    >
+                      <ScrollArea.Thumb className="ScrollAreaThumb" />
+                    </ScrollArea.Scrollbar>
+                    <ScrollArea.Scrollbar
+                      className="ScrollAreaScrollbar"
+                      orientation="horizontal"
+                    >
+                      <ScrollArea.Thumb className="ScrollAreaThumb" />
+                    </ScrollArea.Scrollbar>
+                    <ScrollArea.Corner className="ScrollAreaCorner" />
+                  </ScrollArea.Root>
+                </Popover.Content>
+              </Popover.Portal>
+            </Popover.Root>
           </div>
         </div>
       </Card>
